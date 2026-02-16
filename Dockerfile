f954c15ca95ea7687e2aaf549e1d4161c0683643
ChathuKaveen <chathukavinda12@gmail.com>
2026-02-16 13:13:32 +0530

    added email sending cofiguration with reSend

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY package*.json ./
 
 RUN npm install
 
+RUN npm i strapi-provider-email-resend
+
 COPY . .
 
 RUN npm run build
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    email: {
+      config: {
+        provider: 'strapi-provider-email-resend', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+        providerOptions: {
+          apiKey: env('RESEND_API_KEY'),
+        },
+        settings: {
+            defaultFrom: 'onboarding@resend.dev',
+            defaultReplyTo: 'onboarding@resend.dev',
+        },
+      },
+    },
+    // ...
+  });
